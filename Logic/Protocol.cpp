d8bf55116b8b3f0c5e0a96cfb1e14ed852b034f5
Brazers <eelksnis@yahoo.com>
2017-04-12 16:36:02 +0200

    re

---
+++ Logic/Protocol.cpp
@@ -0,0 +1,18 @@
+/*
+ * Protocol.cpp
+ *
+ *  Created on: Apr 10, 2017
+ *      Author: ee
+ */
+
+#include "Protocol.h"
+
+Protocol::Protocol() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Protocol::~Protocol() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Logic/Protocol.h
@@ -0,0 +1,17 @@
+/*
+ * Protocol.h
+ *
+ *  Created on: Apr 10, 2017
+ *      Author: ee
+ */
+
+#ifndef LOGIC_PROTOCOL_H_
+#define LOGIC_PROTOCOL_H_
+
+class Protocol {
+public:
+	Protocol();
+	virtual ~Protocol();
+};
+
+#endif /* LOGIC_PROTOCOL_H_ */
