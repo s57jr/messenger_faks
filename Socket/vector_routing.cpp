916974f087bbe752e20cf348bd5e032db21d7c0d
Jošt R <jost.reberc@gmail.com>
2017-04-12 09:59:14 +0200

    vector_routing_lessfresh

--- Socket/vector_routing.cpp
+++ Socket/vector_routing.cpp
@@ -1,202 +1,151 @@
 #include "vector_routing.h"
 
-Vector_routing::Vector_routing()
+Vector_routing::Vector_routing():
+    step(0)
 {
 
+
 }
 
 
-int step=0;
 
-void DummyRoutingProtocol::tick(std::vector<client::Packet> packets) {
-    if(step == 0) std::cout << "step s 0" << std::endl;
-    std::cout << "step:" << step << std::endl;
-    // Get the adstd::cout << "step s 0" << std::endl;dress of this node
-    int32_t myAddress = this->linkLayer->getOwnAddress();
-
-    // print something useful for debugging
-    std::cout << "tick; received " << packets.size() << " packets\n";
-    int exception =0;
-
-    int32_t i;
-    // first process the incoming packets; loop over them:
-    for (i = 0; i < packets.size(); i++) {
-        int32_t neighbour = packets[i].getSourceAddress();            // from whom is the packet?
-        int32_t linkcost = this->linkLayer->getLinkCost(neighbour);   // what's the link cost from/to this neighbour?
-        client::DataTable *dt = packets[i].getDataTable();        // other data contained in the packet
-      //  std::cout << "received packet from " << neighbour << " with "
-        //          << dt->getNRows() << " rows and "
-          //        << dt->getNColumns() << " columns of data\n";
+/*  PACKET STRUCTURE
+* type | add1  | add2
+* dest | hop1  | hop2
+*
+*
+*
+*/
 
 
-        protocol::DummyRoute r;
+void Vector_routing::tick(std::vector<std::vector<uint32_t>> packet, std::vector<std::vector<uint32_t> > &pkt_out){
 
 
-        if(linkcost >= 0 || (dt->get(0,0)==3)){			//is there a failed link??
-        if(dt->getNRows() > 0 && (packets.size() != 0)){
-        if(!dt->get(0,0)){
-            if (!(myForwardingTable.count( neighbour ))) {
-                protocol::DummyRoute r;
-                r.nextHop = neighbour;
-                r.cost	  = linkcost;
-                myForwardingTable[neighbour]=r; 		//fill in neighbours
+    for (int32_t i = 0; i < packet.size(); i++) {
 
-            }else{
-                if(myForwardingTable[neighbour].cost > linkcost){
-                    r.nextHop = neighbour;
-                    r.cost = linkcost;
-                    myForwardingTable[neighbour] = r;
+        int32_t neighbour = 123456789;//packet.getSourceAddress();                   // from whom is the packet?
+
+        bool in = false;
+        NInfo curr;
+        curr.address = neighbour;
+        time_t now;
+        time(&now);
+        curr.last = now;
+
+        for(int o=0; o< neighbours.size();o++){
+
+            if((difftime(neighbours[o].last, now))>5){
+                //neighbour disconnected!!
+
+            }
+
+            if(neighbours[o].address == neighbour){
+               in = true;
+               neighbours[o] = curr;
+            }
+        }
+        if(!(in)){                  //add neighbour to the list of neighbours
+            neighbours.push_back(curr);
+        }
+
+        std::cout << "received packet from " << neighbour << " with "
+                  << packet[1].size() << " rows and "
+                  << packet.size() << " columns of data\n";
+
+
+
+        if(!packet[0][0]){                                              //is this broadcast?
+            if (!search_in_row(my_table, 0 , neighbour)){              //already in table?
+                std::vector<uint32_t> tmp ={neighbour, neighbour};
+                my_table.push_back(tmp);
+            }
+        }else if(packet[0][0]){                                         //table packet
+            for(int g =1; g < packet.size();g++){
+                if(search_in_row(my_table, 0 , packet[g][0])){
+                    //the entry is already here
+                }else{                                                  //path through neighbour found, make sure not to advertise it back
+                    if(packet[g][0] != neighbour){
+                        std::vector<uint32_t> blist_col={neighbour, packet[g][0]};
+                        blacklist.push_back(blist_col);
+                    }
+                    std::vector<uint32_t> tmp ={packet[g][0],neighbour};
+                    my_table.push_back(tmp);
                 }
             }
-       }else if(dt->get(0,0)){
-           //start building table
-           for(int g =1; g < dt->getNColumns();g++){
-               if(myForwardingTable.count( dt->get(0,g) ) /*&& (dt->get(0,g) != myAddress)*/){  //if it is in check for lower cost
-                      if(myForwardingTable[dt->get(0,g)].cost > (dt->get(2,g)+linkcost)){
-                          r.nextHop=neighbour;
-                          r.cost =(dt->get(2,g)+linkcost);
-                          myForwardingTable[dt->get(0,g)] = r;
-                      }
-               }else{
-                   //add entry
-                  r.nextHop=neighbour;
-                  r.cost =(dt->get(2,g)+linkcost);
-                  myForwardingTable[dt->get(0,g)] = r;
-               }
-           }
-       }
-       }
-       }else{  //reinitialize the link sice one of the links failed
-
-       std::cout << "dt:" << dt->get(0,0) << std::endl;
-
-       step = 0;
-
-       client::DataTable dta = client::DataTable(1);
-       std::vector<int32_t> a;
-       a.push_back(0);
-       exception = 1;
-       dta.addRow(a);
-
-       dta.set(0,0,3);
-
-       std::cout << "dta:" << dta.get(0,0) << std::endl;
-
-       for(int b =0;b < myForwardingTable.size(); b++){
-
-       }
-       for (std::map<int32_t, DummyRoute>::iterator it = myForwardingTable.begin();it != myForwardingTable.end(); it++) {
-           int32_t destination = it->first;
-           if(destination != neighbour){
-               client::Packet pkt = client::Packet(myAddress, destination, &dta);
-               this->linkLayer->transmit(pkt);
-           }
-
-       }
-
-       myForwardingTable.clear();
-
-       break;
-       }
-    }
-
-
-
-
-
-    client::DataTable dta = client::DataTable(myForwardingTable.size()+1);   // the 3 is the number of columns, you can change this
-
-    if(step == 0 && (exception != 1)){
-        std::vector<int32_t> a,b,c;
-        a.reserve(myForwardingTable.size()+1);
-        b.reserve(myForwardingTable.size()+1);
-        c.reserve(myForwardingTable.size()+1);
-
-        for (int g = 0; g< (myForwardingTable.size()+1); g++){
-            b.push_back(0);
-            c.push_back(0);
-
-            a.push_back(0);
-
         }
 
-        dta.addRow(a);
-        dta.addRow(b);
-        dta.addRow(c);
 
-        std::cout << "step s 0" << std::endl;
+        if(step == 0){
 
-        std::cout << myForwardingTable.size()+1 << std::endl;
+            std::vector<uint32_t> vct;                                                      // first broadcast my address to neighbours
+            vct.insert(vct.begin(),0);
+            vct.shrink_to_fit();        //size should be one here
 
-        dta.set(0,0,0);	// first broadcast my address to neighbours
-        step+=1;
-    }else{
-        std::vector<int> a,b,c;
-        a.reserve(myForwardingTable.size()+1);
-        b.reserve(myForwardingTable.size()+1);
-        c.reserve(myForwardingTable.size()+1);
+            pkt_out.insert(pkt_out.begin(), vct);
+            pkt_out.shrink_to_fit();     //size should be 1 here!!
 
-        for (int g = 0; g< (myForwardingTable.size()+1); g++){
-            b.push_back(0);
-            c.push_back(0);
+            //packet is ready here!!
 
-            a.push_back(0);
+            step+=1;
 
-        }
+        }else{                              //create packet out, note that it is different for some neighbours
 
-        dta.addRow(a);
-        dta.addRow(b);
-        dta.addRow(c);
+            for(int i=0; i < blacklist.size(); i++){
+                std::vector<std::vector<uint32_t>> pkt_to_send;
+                std::vector<uint32_t> first_c = {1, blacklist[i][0]};
+                pkt_to_send.push_back(first_c);
+                for(int j =1 ; j < my_table.size(); j++){
+                       bool blacklisted = false;
+                       for(int a =1; a < blacklist[i].size(); a++){
+                            if(!(my_table[j][0] == blacklist[i][a])){
+                                blacklisted = true;
+                            }
+                       }
+                       if(!(blacklisted)){
+                           pkt_to_send.push_back(my_table[j]);
 
+                           //packet is ready here!
+                       }
+                }
+            }
+            for(int n =0; n<neighbours.size(); n++){
+                bool already_sent = false;
+                for(int z =0; z< blacklist.size();z++){
+                    if(neighbours[n].address == blacklist[z][0]){
+                        already_sent = true;
+                    }
+                    if(!(already_sent)){
+                        std::vector<std::vector<uint32_t>> pkt_to_send;
+                        std::vector<uint32_t> first_c = {1, neighbours[n].address};
+                        pkt_to_send.push_back(first_c);
+                        for(int j =1 ; j < my_table.size(); j++){
+                            pkt_to_send.push_back(my_table[j]);
+                        }
 
-        dta.set(0,0,1);
-        int cnt = 1;
-        for (std::map<int32_t, DummyRoute>::iterator it = myForwardingTable.begin();it != myForwardingTable.end(); it++) {
-            int32_t destination = it->first;
-            int32_t nextHop = it->second.nextHop;
-            int32_t cost	= it->second.cost;
-
-            dta.set(0,cnt,destination);
-            dta.set(1,cnt,nextHop);
-            dta.set(2,cnt,cost);
-
-            cnt+=1;
+                        //packet is ready here!
+                    }
+                }
+            }
         }
     }
-
-    /*
-     * add1  | add2
-     * hop1  | hop2
-     * cost1 | cost2
-     *
-     *
-     *
-     */
-
-
-    if(exception != 1){
-    client::Packet pkt = client::Packet(myAddress, 0, &dta);
-    this->linkLayer->transmit(pkt);
-    }
-
 }
 
-std::map<int32_t, int32_t> DummyRoutingProtocol::getForwardingTable() {
-    // This code transforms your forwarding table which may contain extra information
-    // to a simple one with only a next hop (value) for each destination (key).
-    // The result of this method is send to the server to validate and score your protocol.
-
-    //  <Destination, NextHop>
-    std::map<int32_t, int32_t> ft;
-
-    for (std::map<int32_t, DummyRoute>::iterator it = myForwardingTable.begin();
-         it != myForwardingTable.end(); it++) {
-        int32_t destination = it->first;
-        int32_t nextHop = it->second.nextHop;
-
-        ft[destination] = nextHop;
+bool Vector_routing::search_for(std::vector<uint32_t> vect, uint32_t value){
+    for(int i = 0 ; i < vect.size(); i++){
+        if(vect.at(i) == value){
+            return true;
+        }
     }
+    return false;
+}
 
-    return ft;
+bool Vector_routing::search_in_row(std::vector<std::vector<uint32_t>> vect,int row, uint32_t value){
+    for(int i = 0 ; i < vect.size(); i++){
+        if(vect[i][row] == value){
+            return true;
+        }
     }
+    return false;
+}
+
 
--- Socket/vector_routing.h
+++ Socket/vector_routing.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 
 #include "datatable.h"
 
@@ -13,9 +14,22 @@ class Vector_routing
 public:
     Vector_routing();
 
-    void Vector_routing::tick(std::vector<std::vector<uint8_t>> packet, std::vector<std::vector<uint8_t>> &my_table);
+    void tick(std::vector<std::vector<uint32_t>> packet, std::vector<std::vector<uint32_t>> &pkt_out);
+    bool search_for(std::vector<uint32_t> vect, uint32_t value);
+    bool search_in_row(std::vector<std::vector<uint32_t>> vect,int row, uint32_t value);
 
+    std::vector<std::vector<uint32_t>> my_table;
 
+private:
+    int step;
+
+    struct NInfo{
+        uint32_t address;
+          time_t last;
+    };
+
+    std::vector<std::vector<uint32_t>> blacklist;
+    std::vector<NInfo> neighbours;
 };
 
 #endif // VECTOR_ROUTING_H
